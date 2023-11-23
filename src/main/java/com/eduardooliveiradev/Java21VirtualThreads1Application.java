d8c9f4b6ba4482f7b5658f94d7c51db2bc00f40e
EDUARDO VIEIRA OLIVEIRA <eduardovieiraoliveiraa@gmail.com>
2023-11-23 20:29:42 -0300

    remoção espaço

--- src/main/java/com/eduardooliveiradev/Java21VirtualThreads1Application.java
+++ src/main/java/com/eduardooliveiradev/Java21VirtualThreads1Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Java21VirtualThreads1Application {
 
 	public static void main(String[] args) {
-		
 		SpringApplication.run(Java21VirtualThreads1Application.class, args);
 	}
 
